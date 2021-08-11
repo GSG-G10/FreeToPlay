25a4080c0d7cf7e12323c4d5f7a674ba659dcad2
Raghad-Mezied <rmezied3@gmail.com>
2021-08-11 12:35:09 +0300

    fix eslint errors Relates #11

--- src/handle500.js
+++ src/handle500.js
@@ -1,6 +1,8 @@
-const path = require('path')
+const path = require('path');
+
+// eslint-disable-next-line no-unused-vars
 const handle500 = (error, req, res, next) => {
-  res.status(500).sendFile(path.join(__dirname, "..", "public", "500.html"));
+  res.status(500).sendFile(path.join(__dirname, '..', 'public', '500.html'));
 };
 
 module.exports = handle500;
